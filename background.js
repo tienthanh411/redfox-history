dbac7206864a437e3bf1a84a06816b959574f9a8
Thanh Bui <thanh.bui@nordea.com>
2021-09-19 16:28:13 +0300

    Store and display word meaning

--- background.js
+++ background.js
@@ -1,27 +1,71 @@
 
 const REDFOX_PREFIX = "https://redfoxsanakirja.fi/fi/sanakirja/-/s/fin/eng/";
+const REDFOX_API_PREFIX = 'https://api.redfoxsanakirja.fi/redfox-api/api/basic/query/fin/eng/';
 const STORAGE_KEY = 'word_list';
 const TIME_GAP = 60*1000; // 60 seconds
 
-// function current_date() {
-//     return new Date().toJSON().slice(0, 10);
-// }
+
+function store_word_meaning(word, meaning) {
+    get_stored_item_list(function (item_list) {
+        var item = null;
+        for (var i = 0; i < item_list.length; i++) {
+            if (item_list[i]['word'] == word) {
+                item = item_list[i];
+                break;
+            }
+        }
+        if (item === null) {
+            return;
+        }
+        item['meaning'] = meaning;
+        store_item_list(item_list);
+    });
+}
+
+async function check_and_store_word_meaning(word) {
+    var url = REDFOX_API_PREFIX + word;
+    var response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+        },
+    });
+    if (response.status != 200) {
+        return;
+    }
+
+    const response_json = await response.json(); //extract JSON from the 
+    var meaning = response_json.subtitleResult.query.word2;
+    var unique_meaning = [...new Set(meaning)];
+    store_word_meaning(word, unique_meaning);
+}
+
+function get_stored_item_list(callback) {
+    chrome.storage.local.get(STORAGE_KEY, function(data) {
+        var item_list = [];
+        if (data[STORAGE_KEY] !== undefined) {
+            item_list = data[STORAGE_KEY];
+        }
+        callback(item_list);
+    });
+}
+
+function store_item_list(item_list) {
+    var set_data = {};
+    set_data[STORAGE_KEY] = item_list;
+    console.log(item_list);
+    chrome.storage.local.set(set_data, function () { });
+}
 
 chrome.tabs.onUpdated.addListener(function (_, change_info, tab) {
     if (change_info.status != "complete" || !tab.url.startsWith(REDFOX_PREFIX)) {
         return;
     }
 
-    var word = tab.url.split('/').pop();
-    var current_time = Date.now();
-    console.log("Checking: " + word + " - " + current_time);
+    const word = tab.url.split('/').pop();
+    console.log("Checking: " + word);
 
-    chrome.storage.local.get(STORAGE_KEY, function(data) {
-        var item_list = [];
-        if (data[STORAGE_KEY] !== undefined) {
-            item_list = data[STORAGE_KEY];
-        }
-
+    get_stored_item_list(function(item_list){
         var item = null;
         for (var i = 0; i < item_list.length; i++) {
             if (item_list[i]['word'] == word) {
@@ -34,18 +78,16 @@ chrome.tabs.onUpdated.addListener(function (_, change_info, tab) {
             item['word'] = word;
             item['count'] = 0;
             item['latest'] = 0;
+            item['meaning'] = [];
             item_list.push(item);
         }
-        if (current_time-item['latest'] > TIME_GAP) {
+        var current_time = Date.now();
+        if (current_time - item['latest'] > TIME_GAP) {
             item['count'] += 1;
         }
         item['latest'] = current_time;
-
-        // Store the new list
-        var set_data = {};
-        set_data[STORAGE_KEY] = item_list;
-        console.log(item_list);
-        chrome.storage.local.set(set_data, function() {});
+        store_item_list(item_list);
+        check_and_store_word_meaning(word);
     });
     
 });
--- popup.js
+++ popup.js
@@ -2,13 +2,28 @@ const REDFOX_PREFIX = "https://redfoxsanakirja.fi/fi/sanakirja/-/s/fin/eng/";
 const STORAGE_KEY = 'word_list';
 const MAX_DISPLAYED_WORDS = 30;
 
-function create_word_dom(index, word) {
-    var text = document.createTextNode(decodeURI(word));
+function create_item_dom(index, item) {
+    var url = REDFOX_PREFIX + item.word;
+    
+    var meaning_dom = null;
+    if (item.meaning.length > 0) {
+        meaning_dom = document.createElement('div');
+        meaning_dom.className = 'meaning-container';
+        ul = document.createElement('ul');
+        item.meaning.forEach(function (s) {
+            var li = document.createElement('li');
+            li.textContent = s;
+            ul.appendChild(li);
+        })
+        meaning_dom.appendChild(ul);
+    }
+
+    var text = decodeURI(item.word);
     var text_dom = document.createElement('a');
     text_dom.className = 'word-link'
-    text_dom.appendChild(text);
     text_dom.target = '_blank';
-    text_dom.href = REDFOX_PREFIX + word;
+    text_dom.href = url;
+    text_dom.appendChild(document.createTextNode(text));
 
     var index_dom = document.createElement('span');
     index_dom.textContent = index + ". ";
@@ -17,49 +32,72 @@ function create_word_dom(index, word) {
     ret.className = 'word-container';
     ret.appendChild(index_dom);
     ret.appendChild(text_dom);
+    if (meaning_dom) {
+        ret.appendChild(meaning_dom);
+    }
+    
     return ret;
 }
 
-function create_word_panel() {
+function create_item_panel() {
     var ret = document.createElement('div');
     ret.className = 'float-child';
     return ret;
 }
 
-function display_most_recent(item_list) {
-    if (item_list.length == 0) {
-        return;
-    }
-    item_list.sort(function(a, b) {
-        return b['latest'] - a['latest'];
-    });
-    
-    var word_list_dom = document.getElementById("word-list");
-    
-    var panels = []
-    for (var i = 0; i < 2; i++) {
-        panels[i] = create_word_panel();
-        word_list_dom.appendChild(panels[i]);
-    }
-    
-    var len = Math.min(MAX_DISPLAYED_WORDS, item_list.length);
-    for (var i = 0; i < len; i++) {
-        var word_dom = create_word_dom(i + 1, item_list[i]['word']);
-        if (i < len/2) {
-            panels[0].appendChild(word_dom);
-        } else {
-            panels[1].appendChild(word_dom);
+function get_stored_item_list(callback) {
+    chrome.storage.local.get(STORAGE_KEY, function (data) {
+        var item_list = [];
+        if (data[STORAGE_KEY] !== undefined) {
+            item_list = data[STORAGE_KEY];
         }
-        
-    }
-
+        callback(item_list);
+    });
 }
 
-chrome.storage.local.get(STORAGE_KEY, function(data) {
-    var item_list = [];
-    if (data[STORAGE_KEY] !== undefined) {
-        item_list = data[STORAGE_KEY];
+function remove_all_child_nodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
-    display_most_recent(item_list);
-    
+}
+
+function display_most_recent() {
+    var item_list_dom = document.getElementById("item-list");
+    remove_all_child_nodes(item_list_dom);
+
+    get_stored_item_list(function (item_list) {
+        if (item_list.length == 0) {
+            return;
+        }
+        item_list.sort(function (a, b) {
+            return b['latest'] - a['latest'];
+        });
+
+        var panels = []
+        for (var i = 0; i < 2; i++) {
+            panels[i] = create_item_panel();
+            item_list_dom.appendChild(panels[i]);
+        }
+
+        var len = Math.min(MAX_DISPLAYED_WORDS, item_list.length);
+        for (var i = 0; i < len; i++) {
+            var word_dom = create_item_dom(i + 1, item_list[i]);
+            if (i < len / 2) {
+                panels[0].appendChild(word_dom);
+            } else {
+                panels[1].appendChild(word_dom);
+            }
+        }
+    });
+}
+
+window.addEventListener('load', (event) => {
+    var clear_all_btn = document.getElementById('clear-all-btn');
+    clear_all_btn.addEventListener('click', function () {
+        chrome.storage.local.remove(STORAGE_KEY, function () { });
+        display_most_recent();
+    });
+
+    display_most_recent();
 });
+
